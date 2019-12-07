25587a8f83faf3fa781237e1304fab33cf7beb43
Edgar Cuarezma <ecuarezma@gmail.com>
2019-12-07 17:52:39 -0600

    create react app, add Palette component

---
+++ Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import seedColors from "./seedColors";
+import "./Palette.css";
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        <nav></nav>
+        <div></div>
+        <footer></footer>
+      </div>
+    );
+  }
+}
+
+export default Palette;
