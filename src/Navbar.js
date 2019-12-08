5c51530e61f2cb10d5704c007b26e5ac30e66d42
Edgar Cuarezma <ecuarezma@gmail.com>
2019-12-08 00:20:59 -0600

    add select from material ui to navbar, implement switching formats for colors from select menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ format: e.target.value });
+    this.props.handleChange(e.target.value);
+  }
   render() {
+    const { format } = this.state;
     const { level, changeLevel } = this.props;
     return (
       <nav className="Navbar">
         <div className="logo">
-          <a href="#">react-color</a>
+          <a href="/">react-color</a>
         </div>
         <div className="slider-container">
           <span>Level: {level}</span>
@@ -22,6 +34,13 @@ class Navbar extends Component {
               onAfterChange={changeLevel}
             />
           </div>
+        </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX</MenuItem>
+            <MenuItem value="rgb">RGB</MenuItem>
+            <MenuItem value="rgba">RGBA</MenuItem>
+          </Select>
         </div>
       </nav>
     );
