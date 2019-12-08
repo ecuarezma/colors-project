fdb431d37ec20db18a5e8542a1ef40a3f1447a3a
Edgar Cuarezma <ecuarezma@gmail.com>
2019-12-07 21:36:57 -0600

    style slider

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelper";
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
