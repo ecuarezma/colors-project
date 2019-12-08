cac1942fd0c61a420f569d4064bb2923ac51ba52
Edgar Cuarezma <ecuarezma@gmail.com>
2019-12-07 21:26:36 -0600

    import and implement slider functionality for adjusting color range

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelper";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors[4]} />
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
