7854e96d2c32babaea85742c8aac4383f620fbae
Edgar Cuarezma <ecuarezma@gmail.com>
2019-12-08 19:46:21 -0600

    update route for Palette so it has an :id params linked to palette array in seedColors module

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelper";
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/" render={() => <h1>HOME PAGE</h1>} />
-      <Route exact path="/palette/:id" render={() => <h1>PALETTE</h1>} />
-    </Switch>
-    // <div>
-    //   <Palette palette={generatePalette(seedColors[4])} />
-    // </div>
-  );
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(palette => palette.id === id);
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>HOME PAGE</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+    );
+  }
 }
 
 export default App;
